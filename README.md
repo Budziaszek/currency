bbbd465ea475d5c005c3336b499fd98d41c376f8
Magdalena Budziaszek <budziaszek.m@gmail.com>
2022-06-27 12:44:24 +0200

    Post processing, logging

--- README.md
+++ README.md
@@ -5,3 +5,29 @@
 Requesting results in csv formats requires less formatting. Depending on the requirements I would consider using it,
 but I noticed, that it doesn't support selecting time period. If we would always define relatively short period
 of time I believe that it is better to parse xml.
+
+## Additional data
+I have used loop to get all metadata about series, but then decided to get only the attributes, which I was using.
+(see commit b1c469cb)
+
+```python
+currency_data = {
+    item['@id']: item['@value']
+    for group in ('generic:SeriesKey', 'generic:Attributes')
+    for item in series[group]['generic:Value']
+    if item['@id'] in ('CURRENCY', 'CURRENCY_DENOM',)
+}
+```
+
+## No data available
+When no data is available for a specific day, the day will include the last known values from previous days,
+but there is one exception - currently, if start date given is the data for which the service will not return results.
+I would solve this by running additional queries in post processing (post_process).
+
+## Currency denomination
+It seems that service has only data for EUR denomination. Am I missing something? Should I calculate
+values?
+
+
+
+
--- exchange_rate.py
+++ exchange_rate.py
@@ -1,12 +1,40 @@
+import csv
 import datetime
+import logging
+from collections import defaultdict
+from http import HTTPStatus
+from typing import Dict, Union, List
+from urllib.error import HTTPError
 from urllib.request import urlopen
 
 from xmltodict import parse
 
+logging.getLogger().setLevel(logging.DEBUG)
 
-def get_config(currency: str, currency_denom: str, frequency: str = 'D', exchange_rates_type: str = 'SP00',
-               series_variation: str = 'A'):
-    return '.'.join([frequency, currency, currency_denom, exchange_rates_type, series_variation])
+ERROR_MESSAGES = {
+    HTTPStatus.SEE_OTHER: "No changes since the timestamp supplied in the If-Modified-Since header.",
+    HTTPStatus.BAD_REQUEST: "There is a syntactic or semantic issue with the parameters you supplied.",
+    HTTPStatus.NOT_FOUND: "There are no results matching the query.",
+    HTTPStatus.NOT_ACCEPTABLE: "A resource representation is not supported. See the section about content negotiation, "
+                               "to view the supported representations.",
+    HTTPStatus.INTERNAL_SERVER_ERROR: "There is an issue on server side. "
+                                      "Feel free to try again later or to contact our support hotline.",
+    HTTPStatus.NOT_IMPLEMENTED: "This web service offers a subset of the functionality offered by the SDMX RESTful "
+                                "web service specification. A feature that is not yet implemented.",
+    HTTPStatus.SERVICE_UNAVAILABLE: "Web service is temporarily unavailable",
+}
+
+
+def get_key(currency: Union[str, List], currency_denom: Union[str, List], frequency: str = 'D',
+            exchange_rates_type: str = 'SP00', series_variation: str = 'A'):
+    if isinstance(currency, list):
+        currency = '+'.join(currency)
+    if isinstance(currency_denom, list):
+        currency_denom = '+'.join(currency_denom)
+
+    key = '.'.join([frequency, currency, currency_denom, exchange_rates_type, series_variation])
+    logging.debug(f"Created key: {key}")
+    return key
 
 
 def get_url(series, from_date: datetime, to_date: datetime, csv_data=True):
@@ -16,33 +44,68 @@ def get_url(series, from_date: datetime, to_date: datetime, csv_data=True):
     return base_uri + series + from_date_str + '&' + to_date_str
 
 
-def get_data(data):
-    parsed_data = []
+def was_requested(currency_data, from_currencies, to_currencies):
+    if from_currencies is None or currency_data['CURRENCY'] in from_currencies:
+        if to_currencies is None or currency_data['CURRENCY_DENOM'] in to_currencies:
+            return True
+    return False
+
+
+def get_data(data, from_currencies=None, to_currencies=None):
+    parsed_data = defaultdict(dict)
     for series in data:
-        currency_data = {item['@id']: item['@value']
-                         for group in ('generic:SeriesKey', 'generic:Attributes')
-                         for item in series[group]['generic:Value']
-                         if item['@id'] in ('CURRENCY', 'CURRENCY_DENOM',)
-                         }
-        for item in series['generic:Obs']:
-            row = {
-                'OBS_VALUE': item['generic:ObsValue']['@value'],
-                'OBS_DATE': item['generic:ObsDimension']['@value'],
-                **currency_data
-            }
-            parsed_data.append(row)
+        currency_data = {
+            'CURRENCY': series['generic:SeriesKey']['generic:Value'][1]['@value'],
+            'CURRENCY_DENOM': series['generic:SeriesKey']['generic:Value'][2]['@value']
+        }
+        if was_requested(currency_data, from_currencies, to_currencies):
+            for item in series['generic:Obs']:
+                row = {
+                    'VALUE': item['generic:ObsValue']['@value'],
+                    **currency_data
+                }
+                label = f'{currency_data["CURRENCY"]}-{currency_data["CURRENCY_DENOM"]}'
+                parsed_data[label][item['generic:ObsDimension']['@value']] = row
     return parsed_data
 
 
-def query(series: str,
+def query(key: str,
           from_date: datetime,
           to_date: datetime):
-    with urlopen(get_url(series=series, from_date=from_date, to_date=to_date)) as url:
-        raw = parse(url.read().decode('utf8'))
-    return get_data(data=raw['message:GenericData']['message:DataSet']['generic:Series'])
+    try:
+        with urlopen(get_url(series=key, from_date=from_date, to_date=to_date)) as url:
+            raw = parse(url.read().decode('utf8'))
+    except HTTPError as e:
+        e.msg = ERROR_MESSAGES[e.code]
+        raise
+
+    result = get_data(data=raw['message:GenericData']['message:DataSet']['generic:Series'])
+    logging.debug(f"Request for (key={key}, from_date={from_date}, to_date={to_date}) resulted in {len(result)} items.")
+    return result
+
+
+def post_process(data: Dict, from_date: datetime, to_date: datetime):
+    dates_range = [(from_date + datetime.timedelta(days=d)).strftime('%Y-%m-%d')
+                   for d in range((to_date - from_date).days)]
+
+    yield ['date', 'currency', 'currency_denom', 'value']
+    for key, value in data.items():
+        last = [None, None, None, None]
+        for date in dates_range:
+            if date in value:
+                last = [value[date]['CURRENCY'], value[date]['CURRENCY_DENOM'], value[date]['VALUE']]
+            yield [date, *last]
 
 
 if __name__ == "__main__":
-    c = get_config(currency="", currency_denom="")
-    print(c)
-    print(query(c, from_date=datetime.date.today() - datetime.timedelta(days=7), to_date=datetime.date.today()))
+    key = get_key(currency="", currency_denom="")
+    from_date = datetime.date.today() - datetime.timedelta(days=7)
+    to_date = datetime.date.today()
+
+    result = query(key=key, from_date=from_date, to_date=to_date)
+    result = post_process(data=result, from_date=from_date, to_date=to_date)
+
+    with open("output.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        for row in result:
+            writer.writerow(row)
